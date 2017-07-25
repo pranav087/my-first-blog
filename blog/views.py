c4d17b46790e888330e2376eb9ac2c1b229253d5
pranav087 <pranavagarwal_ak@srmuniv.edu.in>
2017-07-25 17:36:20 +0530

    django app, first commit

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Post
+
+def post_list(request):
+    return render(request, 'blog/post_list.html',{})
+def home(request):
+
+    queryset=Post.objects.all()
+    dta={
+        "queryset":queryset
+    }
+    return render(request, 'blog/home.html', {})
